824f18dcd47933ba0622925316a885548d469c3b
h4321 <42736789+h4321@users.noreply.github.com>
2023-01-19 15:07:29 +0100

    Update AccountNumberTests.cs

--- BankExample/Parasoft.Dottest.Examples.Bank.Tests.MSTest/AccountNumberTests.cs
+++ BankExample/Parasoft.Dottest.Examples.Bank.Tests.MSTest/AccountNumberTests.cs
@@ -34,8 +34,8 @@ namespace Parasoft.Dottest.Examples.Bank.Tests.MSTest
         {
             var accountNumbersCollections = new List<byte[]>
             {
-                new byte[]{1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6},
-                new byte[]{1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6},
+                new byte[]{1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,7},
+                new byte[]{1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,7},
                 new byte[]{0,9,8,7,6,5,4,3,2,1,0,9,8,7,6,5},
                 new byte[]{0,9,8,7,6,5,4,3,2,1,0,9,8,7,6,5}
             };
